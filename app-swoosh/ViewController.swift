0b43a3ff6534ee88e75d245bc3f95281359d47b6
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-10-23 16:40:31 +0500

    First Day of Swoosh

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by MacBook on 23/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
