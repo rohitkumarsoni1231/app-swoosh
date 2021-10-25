b8112bcd48813292d11796614162f5243f646211
Rohit <rohitkumarrsoni1231@gmail.com>
2021-10-25 16:57:49 +0500

    back button segue done

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -11,14 +11,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
     
-        swoosh.frame = CGRect(x: view.frame.size .width / 2 - view.frame.size .width / 2 , y: 50, width: swoosh.frame.size.width , height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
     }
 
