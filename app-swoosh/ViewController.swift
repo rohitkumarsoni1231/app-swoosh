c3a59413dbc3d56dde1ef79fa931b2478aaea8d9
Rohit <rohitkumarrsoni1231@gmail.com>
2021-10-25 14:16:33 +0500

    Set AutoLayout

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -1,19 +1,26 @@
-//
 //  ViewController.swift
 //  app-swoosh
 //
 //  Created by MacBook on 23/10/2021.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        swoosh.frame = CGRect(x: view.frame.size .width / 2 - view.frame.size .width / 2 , y: 50, width: swoosh.frame.size.width , height: swoosh.frame.size.height)
+        
+        bgImg.frame = view.frame
+        
     }
 
-
+    
 }
-
